039ff6d7d26aae93755af0c9bd571211f165637d
Moises <moisesantoniobrasil@gmail.com>
2023-06-27 10:19:31 -0300

    tentativa6

--- src/links/Linkavengers.jsx
+++ src/links/Linkavengers.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import capa1 from "/Users/moise/Desktop/Projetos/REACT/FILMES_CINEMA/filmes_cinema/filmes_cinema/src/capasdefilme/capa1.png"
+import capa1 from "../capasdefilme/capa1.png"
 
 function Linkavengers(){ 
     const navigate = useNavigate()
